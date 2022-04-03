7d44ee4e287ee176db6226c1efa3ddf143a55ca0
bhavishyachandel43 <86842884+bhavishyachandel43@users.noreply.github.com>
2022-04-03 18:53:05 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,49 @@
+function preload() {
+    img=loadImage('fan.jpg');
+}
+function modalLoaded() {
+    console.log("ModalLoaded!");
+    status=true;
+    objectDetector.detect(img, gotResult);
+    
+}
+function gotResult(error, results) {
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+    
+}
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocosed', modalLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+
+    
+}
+img="";
+status="";
+objects=[];
+function draw() {
+    image(img, 0, 0, 640, 420);
+if(status !="")
+{
+  for (i=0; i < objects.length;i++) 
+  {
+    document.getElementById("status").innerHTML="Status.Object Detected";
+    fill("#FF0000");
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+    noFill();
+    stroke("#FF0000");
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+  }
+}
+
+  } 
+
+  function back() {
+    window.location="first.html";
+}
